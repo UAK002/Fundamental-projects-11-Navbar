90756fa6ed68acc4c932bc3d8bc9873177faf4d1
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-14 07:44:23 +0530

    247. Navbar - Question

---
+++ src/Example.jsx
@@ -0,0 +1,53 @@
+import { useRef, useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  //   const linksContainerRef = useRef('null');
+  const linksRef = useRef('null');
+
+  //   console.log(linksRef.current.getBoundingClientRect());
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  const linkStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : `0px`,
+  };
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+
+        {/* <div
+          className="links-container"
+          ref={linksContainerRef}
+          style={linkStyles}
+        ></div> */}
+        <ul className="links" ref={linksRef} style={linkStyles}>
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
