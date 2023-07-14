2a6b8336e51591b0d09c9cb547d873e3e2147e56
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-14 08:58:22 +0530

    248. Navbar - Complete App

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -50,6 +50,17 @@ const Navbar = () => {
             })}
           </ul>
         </div>
+        {/* Social links */}
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );
